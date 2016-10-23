d8ab31853cafd30b484dbe666673a67088782787
Felix Gail <felix.clemens.gail@gmail.com>
2016-10-23 20:31:05 +0200

    pre-train

--- vier_gewinnt.py
+++ vier_gewinnt.py
@@ -1,101 +1,91 @@
-from tkinter import Tk, Frame, Label, Canvas
+from tkinter import Tk, Canvas
 from enum import Enum
 
 
 # Change Color Codes to edit Board GUI
 class Color(Enum):
-    RED_OUTER = '#FF0000'
-    RED_INNER = '#8A0808'
-    YELLOW_OUTER = '#FACC2E'
-    YELLOW_INNER = '#DBA901'
+    PLAYER_1_OUTER = '#FF0000'
+    PLAYER_1_INNER = '#8A0808'
+    PLAYER_2_OUTER = '#FACC2E'
+    PLAYER_2_INNER = '#DBA901'
     WHITE = '#FFFFFF'
     BOARD = '#2E2EFE'
     OUTLINE = '#1C1C1C'
     HIGHLIGHT = '#04B404'
-    RED_HOVERED_OUTER = '#610B0B'
-    RED_HOVERED_INNER = '#3B0B0B'
-    YELLOW_HOVERED_OUTER = '#886A08'
-    YELLOW_HOVERED_INNER = '#5F4C0B'
+    PLAYER_1_HOVERED_OUTER = '#610B0B'
+    PLAYER_1_HOVERED_INNER = '#3B0B0B'
+    PLAYER_2_HOVERED_OUTER = '#886A08'
+    PLAYER_2_HOVERED_INNER = '#5F4C0B'
+    FONT_COLOR_WINNER = '#B40404'
+    FONT_COLOR_WINS = '#FFFFFF'
 
 
-class ClickAble(Frame):
-    def __init__(self, on_enter=None, on_leave=None, on_click=None, *args, **kwargs):
-        Frame.__init__(self, *args, **kwargs)
-        self.on_enter_call = on_enter
-        self.on_leave_call = on_leave
-        self.on_click_call = on_click
-        self.bind('<Enter>', self.on_enter)
-        self.bind('<Leave>', self.on_leave)
+class PlayerType(Enum):
+    PLAYER_1 = 'RED'
+    PLAYER_2 = 'YELLOW'
 
-    def __del__(self):
-        self.unbind('<Enter')
-        self.unbind('Leave')
 
-    def on_enter(self, event):
-        if hasattr(self.on_click_call, '__call__'):
-            self.bind('<Button-1>', self.on_click_call)
-        if hasattr(self.on_enter_call, '__call__'):
-            self.on_enter_call(event)
-
-    def on_leave(self, event):
-        if hasattr(self.on_click_call, '__call__'):
-            self.unbind('<Button-1>')
-        if hasattr(self.on_leave_call, '__call__'):
-            self.on_leave_call(event)
+class FieldStatus(Enum):
+    FREE = 0
+    OCCUPIED = 1
 
 
 class Player(object):
-    def __init__(self, color):
+    def __init__(self, player: PlayerType):
         self.points = 0
         self.next_player = None
-        if color == 'red':
-            self.color_outer = Color.RED_OUTER.value
-            self.color_inner = Color.RED_INNER.value
-            self.color_hovered_outer = Color.RED_HOVERED_OUTER.value
-            self.color_hovered_inner = Color.RED_HOVERED_INNER.value
-        elif color == 'yellow':
-            self.color_outer = Color.YELLOW_OUTER.value
-            self.color_inner = Color.YELLOW_INNER.value
-            self.color_hovered_outer = Color.YELLOW_HOVERED_OUTER.value
-            self.color_hovered_inner = Color.YELLOW_HOVERED_INNER.value
+        self.player_type = player
+        if player == PlayerType.PLAYER_1:
+            self.color_outer = Color.PLAYER_1_OUTER.value
+            self.color_inner = Color.PLAYER_1_INNER.value
+            self.color_hovered_outer = Color.PLAYER_1_HOVERED_OUTER.value
+            self.color_hovered_inner = Color.PLAYER_1_HOVERED_INNER.value
+        elif player == PlayerType.PLAYER_2:
+            self.color_outer = Color.PLAYER_2_OUTER.value
+            self.color_inner = Color.PLAYER_2_INNER.value
+            self.color_hovered_outer = Color.PLAYER_2_HOVERED_OUTER.value
+            self.color_hovered_inner = Color.PLAYER_2_HOVERED_INNER.value
         else:
-            raise ValueError('Playercolor "{}" not recognized'.format(color))
+            raise ValueError('PlayerType "{}" not recognized'.format(player))
 
     def add_point(self):
         self.points += 1
 
 
 class Game(object):
-    def __init__(self, columns=7, rows=6, field_radius=50, field_space=10):
+    def __init__(self, columns=2, rows=2, field_radius=100, field_space=50):
         self.columns = columns
         self.rows = rows
         self.board = None
         self.canvas = None
         self.hovered = None
-        self.player1 = Player('red')
-        self.player2 = Player('yellow')
+        self.canvas_text_winner_id = None
+        self.canvas_text_wins_id = None
+        self.player1 = Player(PlayerType.PLAYER_1)
+        self.player2 = Player(PlayerType.PLAYER_2)
         self.player1.next_player = self.player2
         self.player2.next_player = self.player1
         self.active_player = self.player1
         self.field_radius = field_radius
         self.field_space = field_space
+        self.canvas_width = (self.columns + 1) * self.field_space + 2 * self.columns * self.field_radius
+        self.canvas_height = (self.rows + 1) * self.field_space + 2 * self.rows * self.field_radius
         tk = Tk()
-        tk.geometry('1180x770')
+        tk.geometry('{}x{}'.format(self.canvas_width, self.canvas_height))
         tk.title('4 Gewinnt')
-        self.initiate_board(tk, 200, 50)
+        self.initiate_board(tk)
+        self.initiate_win_labels()
         self.bind_events()
         tk.mainloop()
 
-    def initiate_board(self, master, x, y):
-        width = (self.columns + 1) * self.field_space + 2 * self.columns * self.field_radius
-        height = (self.rows + 1) * self.field_space + 2 * self.rows * self.field_radius
-        self.create_canvas(master, x, y, width, height)
+    def initiate_board(self, master):
+        self.create_canvas(master)
         self.board = []
         start = self.field_space + self.field_radius
         add = 2 * self.field_radius + self.field_space
         x_pos = start
         for column in range(self.columns):
-            y_pos = height - start
+            y_pos = self.canvas_height - start
             inner = []
             for row in range(self.rows):
                 field_object = Field(x_pos, y_pos, self.field_radius, self.canvas, column, row)
@@ -105,21 +95,32 @@ class Game(object):
             self.board.append(inner)
             x_pos += add
 
-    def create_canvas(self, master, x, y, width, height):
-        self.canvas = Canvas(master=master, bg=Color.BOARD.value)
-        self.canvas.place(x=x, y=y, width=width, height=height)
+    def initiate_win_labels(self):
+        half_width = int(self.canvas_width/2)
+        one_quarter_height = int(self.canvas_height/6)
+        self.canvas_text_winner_id = self.canvas.create_text(half_width, one_quarter_height*2,
+                                                             font='Helvetica 20 bold',
+                                                             fill=Color.FONT_COLOR_WINNER.value)
+        self.canvas_text_wins_id = self.canvas.create_text(half_width, one_quarter_height*3,
+                                                           font='Helvetica 16 bold', fill=Color.FONT_COLOR_WINS.value)
+
+    def create_canvas(self, master):
+        self.canvas = Canvas(master=master, bg=Color.BOARD.value, highlightthickness=0)
+        self.canvas.place(x=0, y=0, width=self.canvas_width, height=self.canvas_height)
 
     def bind_events(self):
-        self.canvas.bind('<Enter>', self.on_enter)
-        self.canvas.bind('<Leave>', self.on_leave)
-
-    def unbind_event(self):
-        self.canvas.unbind('<Enter>')
-        self.canvas.unbind('<Leave>')
-
-    def on_enter(self, event):
-        self.canvas.bind('<Motion>', self.on_motion)
         self.canvas.bind('<Button-1>', self.on_click)
+        self.canvas.bind('<Motion>', self.on_motion)
+
+    def after_win_events(self):
+        self.canvas.bind('<Button-1>', self.reset_binds)
+        self.canvas.unbind('<Motion>')
+
+    def reset_binds(self, event):
+        self.canvas.itemconfigure(self.canvas_text_wins_id, text='')
+        self.canvas.itemconfigure(self.canvas_text_winner_id, text='')
+        self.reset_board()
+        self.bind_events()
 
     def on_motion(self, event):
         current_percent_right = \
@@ -139,31 +140,37 @@ class Game(object):
     def on_click(self, event):
         if self.hovered is not None:
             self.hovered.claim(self.active_player)
-            if not self.is_end():
-                self.active_player = self.active_player.next_player
+            self.is_end()
+            self.active_player = self.active_player.next_player
             self.hovered = None
-
-    def on_leave(self, event):
-        if self.hovered is not None:
-            self.hovered.reset()
-            self.hovered = None
-        self.canvas.unbind('<Motion>')
-        self.canvas.unbind('<Button-1>')
 
     def is_end(self):
         winner = None
         claimed = self.hovered
-        claimed_row = claimed.row
-        claimed_column = claimed.column
-        # TODO: Win Detection
-        for column in self.board:
+
+        directions = [[0, 1], [1, 0], [1, 1]]
+        for direction in directions:
+            streak = self.get_occupied_streak(claimed, direction[0], direction[1])
+
+        for column in self.board: #detect if board is full without anyone winning
             last_item = column[-1]
             if last_item.status == FieldStatus.FREE:
-                return False
+                return
         self.end_game(winner)
 
+    def get_occupied_streak(self, field, add_x, add_y):
+        return []
+
     def end_game(self, winner):
-        self.reset_board()
+        winner = self.player1
+        self.after_win_events()
+        if winner is not None:
+            text = '{} WINS'.format(winner.player_type.value)
+            winner.add_point()
+            self.canvas.itemconfigure(self.canvas_text_wins_id, text='Wins: {}'.format(winner.points))
+        else:
+            text = 'NOBODY WINS'
+        self.canvas.itemconfigure(self.canvas_text_winner_id, text=text)
 
     def reset_board(self):
         for column in self.board:
@@ -208,11 +215,6 @@ class Field(object):
 
     def outline(self):
         self.canvas.itemconfigure(self.outer_circle_id, outline=Color.HIGHLIGHT.value, width=5)
-
-
-class FieldStatus(Enum):
-    FREE = 0
-    OCCUPIED = 1
 
 
 def _create_circle(self, x, y, r, **kwargs):
